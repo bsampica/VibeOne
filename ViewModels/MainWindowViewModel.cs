5d7cd069d3a86bbdefa10a0a9df31ce725f342b6
= <bryansampica@hotmail.com>
2023-08-21 16:53:52 -0400

    Working navigation, and loading animation.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using DynamicData;
 using ReactiveUI;
 
@@ -6,45 +9,73 @@ namespace VibeNine.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private readonly PageViewModelBase[] Pages =
+    private bool _progressVisible = true;
+    private bool _contentVisible = false;
+    public bool ProgressVisible
+    {
+        get => _progressVisible;
+        set => this.RaiseAndSetIfChanged(ref _progressVisible, value);
+    }
+    public bool ContentVisible
+    {
+        get => _contentVisible;
+        set => this.RaiseAndSetIfChanged(ref _contentVisible, value);
+    }
+
+    private readonly PageViewModelBase[] _pages =
     {
         new HomePageViewModel(),
         new TankDetailsViewModel()
     };
 
     private PageViewModelBase _currentPage;
-
     public PageViewModelBase CurrentPage
     {
         get => _currentPage;
         private set => this.RaiseAndSetIfChanged(ref _currentPage, value);
     }
+    public ICommand NavigateHomeCommand { get; }
+    public ICommand NavigateOperationsCommand { get; }
 
-    public ICommand NavigateNextCommand { get; }
-    public ICommand NavigatePreviousCommand { get; }
+    private int _progressPercent = 0;
+
+    public int ProgressPercent
+    {
+        get => _progressPercent;
+        set => this.RaiseAndSetIfChanged(ref _progressPercent, value);
+    }
 
     public MainWindowViewModel()
     {
-        _currentPage = Pages[0];
+        _currentPage = _pages[0];
         //var canNavNext = this.WhenAnyValue(x => x.CurrentPage.)
 
-        NavigateNextCommand = ReactiveCommand.Create(NavigateNext);
-        NavigatePreviousCommand = ReactiveCommand.Create(NavigatePrevious);
+        NavigateHomeCommand = ReactiveCommand.Create(NavigateHomeView);
+        NavigateOperationsCommand = ReactiveCommand.Create(NavigateOperationsView);
+
+        Task.Run(async () =>
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                ProgressPercent = i;
+                await Task.Delay(100);
+            }
+
+            await Task.Delay(500);
+            this.ProgressVisible = false;
+            this.ContentVisible = true;
+        });
     }
 
-    private void NavigateNext()
+
+    private void NavigateHomeView()
     {
-        // get the current index and add one.  This wont work for specific menu items, where we will get the name of the link and instanciate the viewmodel
-        var index = Pages.IndexOf(CurrentPage) + 1;
-        CurrentPage = Pages[index];
+        if (_currentPage is HomePageViewModel) return;
+        CurrentPage = _pages.First(p => p is HomePageViewModel);
     }
 
-    private void NavigatePrevious()
+    private void NavigateOperationsView()
     {
-        // get the current index and subtract 1
-        var index = Pages.IndexOf(CurrentPage) - 1;
-
-        //  /!\ Be aware that we have no check if the index is valid. You may want to add it on your own. /!\
-        CurrentPage = Pages[index];
+        throw new NotImplementedException();
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,9 @@
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Material.Icons;
+using Material.Icons.Avalonia;
 
 namespace VibeNine.Views;
 
@@ -8,4 +13,18 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
-}
+
+    private void ToggleMenu_OnClick(object? sender, RoutedEventArgs e)
+    {
+        this.SplitViewMenu.IsPaneOpen = !this.SplitViewMenu.IsPaneOpen;
+        if (this.SplitViewMenu.IsPaneOpen)
+        {
+            if ((sender as Button)?.Content is MaterialIcon currentButtonContent) 
+                currentButtonContent.Kind = MaterialIconKind.HamburgerOpen;
+        }
+        else
+        {
+            if ((sender as Button)?.Content is MaterialIcon currentButtonContent) 
+                currentButtonContent.Kind = MaterialIconKind.HamburgerClose;
+        }
+    }}
