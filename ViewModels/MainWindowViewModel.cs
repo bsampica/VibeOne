bb2a2f8b1d98caae049db534940f36901aecbbac
= <bryansampica@hotmail.com>
2023-08-22 17:55:22 -0400

    Update the implemention slightly for standards.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,22 +1,14 @@
-﻿using System;
-using System.Linq;
-using System.Reactive;
+﻿using System.Reactive;
 using System.Reflection;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using DynamicData;
 using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
 using Splat;
-using VibeNine.Views;
 
 namespace VibeNine.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase, IScreen
 {
-    public override string? UrlPathSegment { get => "MainWindow"; }
-    public override IScreen? HostScreen { get => throw new NotImplementedException(); }
-
+    public override string UrlPathSegment { get => "MainWindow"; }
+    public override IScreen HostScreen { get; } = null!;
     public RoutingState Router { get; }
     public ReactiveCommand<Unit, IRoutableViewModel> NavigateToOperations { get; }
     public ReactiveCommand<Unit, IRoutableViewModel> NavigateToHomeScreen { get; }
@@ -25,14 +17,7 @@ public class MainWindowViewModel : ViewModelBase, IScreen
     {
         Router = new RoutingState();
         Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
-
-        // Manage the routing state. Use the Router.Navigate.Execute
-        // command to navigate to different view models. 
-        //
-        // Note, that the Navigate.Execute method accepts an instance 
-        // of a view model, this allows you to pass parameters to 
-        // your view models, or to reuse existing view models.
-        //
+      
         NavigateToOperations = ReactiveCommand.CreateFromObservable(() =>
             Router.Navigate.Execute(ExecuteNavOperation()));
 
