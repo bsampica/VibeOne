3d98dfcde12fb8e4dc3c447bf53ff239a3a741d0
= <bryansampica@hotmail.com>
2023-08-23 19:47:21 -0400

    Prototyping the Tank Details View Page, and the charts on the page for the gauge.

--- ViewModels/TankDetailsViewModel.cs
+++ ViewModels/TankDetailsViewModel.cs
@@ -1,12 +1,29 @@
 using System;
+using System.Collections.Generic;
 using System.Reactive;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.Kernel;
+using LiveChartsCore.Measure;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Extensions;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 using Splat;
 
 namespace VibeOne.ViewModels;
 
 public class TankDetailsViewModel : PageViewModelBase
 {
+    public IEnumerable<ISeries> Series { get; set; } = new GaugeBuilder()
+        .WithOffsetRadius(5)
+        .WithLabelsPosition(PolarLabelsPosition.Start)
+        .WithMaxColumnWidth(15)
+        .AddValue(29, "Actual")
+        .AddValue(-4, "SetPoint")
+        .BuildSeries();
+
+
     public override string? UrlPathSegment { get => "TankDetailsView"; }
     private RoutingState Router { get => Locator.Current.GetService<RoutingState>()!; }
 
