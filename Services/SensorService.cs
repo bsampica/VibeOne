0cde68681892d46b8dcaf5cea68cb7e5470a2c2c
= <bryansampica@hotmail.com>
2023-09-04 18:47:06 -0400

    rework the device class slightly.

--- Services/SensorService.cs
+++ Services/SensorService.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Device.Gpio;
+using System.Linq;
 using Avalonia.Threading;
 using Iot.Device.OneWire;
 
@@ -10,44 +11,17 @@ namespace VibeOne.Services;
 
 public class SensorService
 {
-    private const string BusId1 = "w1_bus_master1";
-    private const string BusId2 = "w1_bus_master2";
-    private const string TempSensorOneId = "28-031397944f32"; //28-03139794691e
-    private const string TempSensorTwoId = "28-03139794691e";
-    private OneWireBus _oneWireBus1 { get; set; }
-    private OneWireBus _oneWireBus2 { get; set; }
-    private OneWireThermometerDevice _wireThermometerDevice1 { get; set; }
-    private OneWireThermometerDevice _wireThermometerDevice2 { get; set; }
-    private DispatcherTimer _timer;
+    private const string Device1Id = "28-03139794691e";
+    private const string Device2Id = "28-031397944f32";
 
 
     public SensorService()
     {
         if (DeviceInstance.IsRPI == false) return;
+        var sensor1 = OneWireThermometerDevice.EnumerateDevices().First(ed => ed.DeviceId == Device1Id);
+        var sensor2 = OneWireThermometerDevice.EnumerateDevices().First(ed => ed.DeviceId == Device2Id);
 
-        _oneWireBus1 = new OneWireBus(BusId1);
-        _oneWireBus2 = new OneWireBus(BusId2);
-
-        foreach (var dev in _oneWireBus1.EnumerateDeviceIds())
-        {
-            Console.WriteLine($"Device Id {dev}");
-        }
-
-        foreach (var dev in _oneWireBus2.EnumerateDeviceIds())
-        {
-            Console.WriteLine($"Device Id {dev}");
-        }
-
-        _wireThermometerDevice1 = new OneWireThermometerDevice(BusId1, TempSensorOneId);
-        _wireThermometerDevice2 = new OneWireThermometerDevice(BusId2, TempSensorTwoId);
-
-        Console.WriteLine($"Device One:  {_wireThermometerDevice1.DeviceId}");
-        Console.WriteLine($"Device Two: {_wireThermometerDevice2.DeviceId}");
-
-        var tempReading1 = _wireThermometerDevice1.ReadTemperature();
-        var tempReading2 = _wireThermometerDevice2.ReadTemperature();
-        
-        // Console.WriteLine($"Temp 1 Temp  : {_wireThermometerDevice1.ReadTemperature().DegreesFahrenheit.ToString()}");
-        // Console.WriteLine($"Temp 2 Temp : {_wireThermometerDevice2.ReadTemperature().DegreesFahrenheit.ToString()}");
+        Console.WriteLine($"Sensor 1: {sensor1.ReadTemperature().DegreesFahrenheit}");
+        Console.WriteLine($"SEnsor 2: {sensor2.ReadTemperature().DegreesFahrenheit}");
     }
 }
