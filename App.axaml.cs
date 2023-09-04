3cfac94f2bc37a83891b032e8365c146f11594cb
= <bryansampica@hotmail.com>
2023-09-04 17:36:57 -0400

    Checking to see if we are on a raspberry pi and setting a global variable for now.

--- App.axaml.cs
+++ App.axaml.cs
@@ -14,6 +14,11 @@ using VibeOne.Views;
 
 namespace VibeOne;
 
+public static class DeviceInstance
+{
+    public static bool IsRPI { get; set; } = false;
+}
+
 public partial class App : Application
 {
     public override void Initialize()
@@ -21,22 +26,25 @@ public partial class App : Application
         AvaloniaXamlLoader.Load(this);
         Locator.CurrentMutable.RegisterLazySingleton(() => new RoutingState());
         Locator.CurrentMutable.RegisterLazySingleton(() => new TankService());
+        Locator.CurrentMutable.RegisterLazySingleton(() => new SensorService());
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            DeviceInstance.IsRPI = false;
             Console.WriteLine($"Detected Classic Destkop Style Application Lifetime");
             desktop.MainWindow = new MainWindow() { DataContext = new MainWindowViewModel() };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleView)
         {
+            DeviceInstance.IsRPI = true;
             Console.WriteLine($"Detected Single View Style Application Lifetime");
             singleView.MainView = new MainView() { DataContext = new MainWindowViewModel() };
         }
 
-
+        var sensorService = Locator.Current.GetService<SensorService>();
         base.OnFrameworkInitializationCompleted();
     }
 }
---
+++ Services/SensorService.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+using System.Device.Gpio;
+using Avalonia.Threading;
+using Iot.Device.OneWire;
+
+namespace VibeOne.Services;
+
+public class SensorService
+{
+    private OneWireBus _oneWireBus { get; set; }
+    private OneWireThermometerDevice _wireThermometerDevice { get; set; }
+    private DispatcherTimer _timer;
+
+
+    public SensorService()
+    {
+        if (DeviceInstance.IsRPI == false) return;
+        var busIds = OneWireBus.EnumerateBusIds();
+        foreach (var id in busIds)
+        {
+            Console.WriteLine($"BUS ID: {id}");
+        }
+
+    }
+}
