cfac6913f266e255778f2270b202dfc8cc9464d8
= <bryansampica@hotmail.com>
2023-09-06 20:00:58 -0400

    patch up some code standards.

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -15,25 +15,19 @@ namespace VibeOne.ViewModels;
 public class HomePageViewModel : ViewModelBase, IRoutableViewModel
 {
     public string? UrlPathSegment { get; } = "/home";
-#pragma warning disable CS8766 // Nullability of reference types in return type doesn't match implicitly implemented member (possibly because of nullability attributes).
-    public IScreen? HostScreen { get; }
-#pragma warning restore CS8766 // Nullability of reference types in return type doesn't match implicitly implemented member (possibly because of nullability attributes).
+    public IScreen HostScreen { get; }
     private RoutingState? Router { get; }
     private TankService TankService { get; } = Locator.Current.GetService<TankService>()!;
     [Reactive] public IEnumerable<TankModel>? TankModels { get; set; }
     public ReactiveCommand<Unit, IRoutableViewModel>? NavigateTankDetails { get; }
 
-    public HomePageViewModel()
-    {
-    }
-
-    public HomePageViewModel(IScreen? screen, RoutingState? router)
+    public HomePageViewModel(IScreen screen, RoutingState? router)
     {
         HostScreen = screen;
         Router = router;
         TankModels = TankService.Tanks;
 
         NavigateTankDetails = ReactiveCommand.CreateFromObservable(() =>
-            Router?.Navigate.Execute(new TankDetailsViewModel(HostScreen))!);
+            Router?.Navigate.Execute(new TankDetailsViewModel(screen))!);
     }
 }
